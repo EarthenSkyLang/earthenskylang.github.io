13e5d7cd7adc30670223578a068475e7f9f0e11c
EarthenSky <gabegames27@gmail.com>
2017-10-15 23:20:35 -0700

    Added Bottom Bar

--- js/jsMain.js
+++ js/jsMain.js
@@ -1,3 +1,20 @@
+var levelText = document.getElementById("lvl");
+var currentLevel = 0;
+
 function start() {
   console.log("hi, よ!");
+}
+
+document.addEventListener('keydown', function(event) {
+    if(event.keyCode == 37) {
+        incLvl(1);
+    }
+    else if(event.keyCode == 39) {
+        incLvl(-1);
+    }
+});
+
+function incLvl(val) {
+  currentLevel += val;
+  levelText.innerHTML = "level: " + currentLevel;
 }
