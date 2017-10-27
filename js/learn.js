cb6e764900849902b88bb693b9625784cb82a3a0
EarthenSky <gabegames27@gmail.com>
2017-10-26 21:01:57 -0700

    changes yo!

--- js/learn.js
+++ js/learn.js
@@ -1,2 +1,32 @@
 var inputElement;
 var textElement;
+var nextElement;
+
+var list;
+
+var currentWord = 0;
+
+function lessonStart() {
+  var toDestroy = document.getElementById("startButton");
+  toDestroy.parentNode.removeChild(toDestroy);
+
+  /*needed? vvv*/
+  nextElement.style.visibility = "hidden";
+  inputElement.style.visibility = "hidden";
+
+  currentWord = 0;
+
+  console.log(getHouseWord("book").kana);
+
+  teachWord(currentWord);
+}
+
+var wordsList;
+function teachWord(index) {
+  textElement.innerHTML = wordsList[index] + " -> " + getHouseWord(wordsList[index]).kana + " -> " + getHouseWord(wordsList[index]).romanji + "</br>" + getHouseWord(wordsList[index]).desc;
+  nextElement.style.visibility = "visible";
+}
+
+function repeatWord(index) {
+
+}
--- jsResources/HouseWords.js
+++ jsResources/HouseWords.js
@@ -1,35 +1,74 @@
-//holds words about things in a house
+//holds words about things in a house.
+
+var percentComplete = 0;
+var houseIntroText = "This lesson teaches you some of the objects you can find in your house.";
 
 function lesson1Init () {
-  //var id = document.getElementById("lessonPage1");
-  //inputElement = id.getElementsByTagName("input");
-  //textElement = id.getElementsByTagName("text");
+  //add page variables to lesson script.
+  var id = document.getElementById("lessonPage1");
+  inputElement = id.getElementsByClassName("inputFinish")[0];
+  nextElement = id.getElementsByClassName("acceptFinish")[0];
+  textElement = id.getElementsByClassName("text")[0];
 
+  //add word list to lesson script.
   randomizeHouseWordList();
+  wordsList = engHouseWordsScrambled;
 
-  console.log(engHouseWordsScrambled);
-  console.log(engHouseWords);
-
-  for (var i = 0, len = engHouseWordsScrambled.length; i < len; i++) {
-    document.getElementById("lessonPage1").getElementsByClassName("text")[0].innerHTML += engHouseWords[i] + " : " + engHouseWordsScrambled[i] + " </br>";
-  }
+  textElement.innerHTML = houseIntroText + '<button id="startButton" style="display: block; width: 100px; position: absolute; left: 50%; margin-right: -50%; transform: translate(-50%, 25%)" onclick="lessonStart()">start lesson</button>';
 }
 
 function getHouseWord(en) {
   return houseWords[en];
 }
 
+//holds word translations and word info.
 var houseWords = {
   "book": {
     kana: "本",
-    Romanji:"ho-n",
+    romanji:"ho-n",
     desc: "<b>Words 本 is included in:</b> <br/> 本だな -> (ho-n-da-na) -> book. <br/><br/> <b>Other uses:</b> <br/> counting blades of grass or tree trunks, <br/> counting falcons. <br/> <pronounced mo-to> <br/><br/> counting long cylyndrical objects, <br/> counting films. <br/> <pronounced ho-n>",
     understanding: 0
   },
   "floor": {
     kana: "ゆか",
-    Romanji:"yu-ka",
+    romanji:"yu-ka",
     desc: "means floor",
+    understanding: 0
+  },
+  "table": {
+    kana: "",
+    romanji:"",
+    desc: "",
+    understanding: 0
+  },
+  "chair": {
+    kana: "",
+    romanji:"",
+    desc: "",
+    understanding: 0
+  },
+  "cup": {
+    kana: "",
+    romanji:"",
+    desc: "",
+    understanding: 0
+  },
+  "stool": {
+    kana: "",
+    romanji:"",
+    desc: "",
+    understanding: 0
+  },
+  "door": {
+    kana: "",
+    romanji:"",
+    desc: "",
+    understanding: 0
+  },
+  "window": {
+    kana: "",
+    romanji:"ma-do",
+    desc: "means window",
     understanding: 0
   }
 }
