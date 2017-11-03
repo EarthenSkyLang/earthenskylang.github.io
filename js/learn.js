356956995a168d224dc099faff7166abbc743faf
EarthenSky <gabegames27@gmail.com>
2017-11-02 22:11:33 -0700

    made pictures show randomly.

--- js/learn.js
+++ js/learn.js
@@ -29,22 +29,81 @@ function teachWord() {
   nextElement.onclick = repeatWord;
 }
 
+var pictureWord;
 function repeatWord() {
-  inputElement.getElementsByClassName("img1")[0].src = getHouseWord(wordsList[currentWord]).link;
-  inputElement.getElementsByClassName("img1Text")[0].innerHTML = wordsList[currentWord];
+  //load 4 random pictures (make sure no duplicates)
+  do {
+    wordPic1 = getRandomHouseNumber();
+  } while(wordPic1 === currentWord)
+
+  do {
+    wordPic2 = getRandomHouseNumber();
+  } while(wordPic2 === currentWord)
+
+  do {
+    wordPic3 = getRandomHouseNumber();
+  } while(wordPic3 === currentWord)
+
+  do {
+    wordPic4 = getRandomHouseNumber();
+  } while(wordPic4 === currentWord)
+
+  //make one of them correct
+  var randomPic = Math.floor((Math.random() * 4));
+  if(randomPic == 0) {
+    wordPic1 = currentWord;
+  } else if(randomPic == 1) {
+    wordPic2 = currentWord;
+  } else if(randomPic == 2) {
+    wordPic3 = currentWord;
+  } else if(randomPic == 3) {
+    wordPic4 = currentWord;
+  }
+
+  inputElement.getElementsByClassName("img1")[0].src = getHouseWord(wordsList[wordPic1]).link;
+  inputElement.getElementsByClassName("img1Text")[0].innerHTML = wordsList[wordPic1];
+
+  inputElement.getElementsByClassName("img2")[0].src = getHouseWord(wordsList[wordPic2]).link;
+  inputElement.getElementsByClassName("img2Text")[0].innerHTML = wordsList[wordPic2];
+
+  inputElement.getElementsByClassName("img3")[0].src = getHouseWord(wordsList[wordPic3]).link;
+  inputElement.getElementsByClassName("img3Text")[0].innerHTML = wordsList[wordPic3];
+
+  inputElement.getElementsByClassName("img4")[0].src = getHouseWord(wordsList[wordPic4]).link;
+  inputElement.getElementsByClassName("img4Text")[0].innerHTML = wordsList[wordPic4];
 
   textElement.innerHTML = "What is " + getHouseWord(wordsList[currentWord]).kana + " (" + getHouseWord(wordsList[currentWord]).romanji + ") in english."
 
   inputElement.style.visibility = "visible";
   nextElement.style.visibility = "hidden";
 
-  inputElement.onclick = checkCorrectAnswer;
+  //inputElement.onclick = checkCorrectAnswer;
+  nextElement.onclick = null;
 }
 
-function checkCorrectAnswer() {
-  var answerCorrect = false;
+//gives checkCorrectAnswer() the value each thing is associated with.
+var wordPic1 = "";
+function onClickPicture1() {
+  checkCorrectAnswer(wordPic1);
+}
 
-  if(answerCorrect === true) {
+var wordPic2 = "";
+function onClickPicture2() {
+  checkCorrectAnswer(wordPic2);
+}
+
+var wordPic3 = "";
+function onClickPicture3() {
+  checkCorrectAnswer(wordPic3);
+}
+
+var wordPic4 = "";
+function onClickPicture4() {
+  checkCorrectAnswer(wordPic4);
+}
+
+function checkCorrectAnswer(guessWord) {
+  if(guessWord === currentWord) {
     currentWord++;
     teachWord();
   }
